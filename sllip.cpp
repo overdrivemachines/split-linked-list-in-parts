d1b47b2182e36b8cc1917d9d16b6e78c99643853
get.dipen@gmail.com <get.dipen@gmail.com>
2022-10-29 22:02:35 -0700

    added print linked list function

--- sllip.cpp
+++ sllip.cpp
@@ -3,17 +3,41 @@
 
 using namespace std;
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+  ListNode(int *arr) {
+    // for (int i = 0; i < (*arr).size(); i++) {
+    // }
+  }
+};
 
-int main(int argc, char const* argv[]) {
+vector<ListNode *> splitListToParts(ListNode *head, int k);
+void printLinkedList(ListNode *head);
+
+int main(int argc, char const *argv[]) {
+  ListNode *start = new ListNode(5);
+  printLinkedList(start);
   return 0;
+}
+
+vector<ListNode *> splitListToParts(ListNode *head, int k) {
+  vector<ListNode *> linkedListParts;
+  return linkedListParts;
+}
+
+// Print a linked list from head to tail
+// eg. 4->5->7>2->NULL would be the output
+void printLinkedList(ListNode *head) {
+  ListNode *element;
+
+  element = head;
+  while (element != nullptr) {
+    cout << element->val << "->";
+    element = element->next;
+  }
+  cout << "NULL" << endl;
 }
